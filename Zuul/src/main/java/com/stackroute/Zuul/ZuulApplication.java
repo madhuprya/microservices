6e0902539b09608cb01f54daa3b5562d5a412a5b
madhupriya <madpriy1@in.ibm.com>
2019-01-24 15:23:40 +0530

    integrated zuul

--- Zuul/src/main/java/com/stackroute/Zuul/ZuulApplication.java
+++ Zuul/src/main/java/com/stackroute/Zuul/ZuulApplication.java
@@ -6,10 +6,12 @@ import com.stackroute.Zuul.filters.PreFilter;
 import com.stackroute.Zuul.filters.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableZuulProxy
 public class ZuulApplication {
 
