41d382ee2f95db8750f9896acddab26f6f7d0c42
madhupriya <madpriy1@in.ibm.com>
2019-01-25 20:43:15 +0530

    final commit

--- MuzixApplicationTrack/src/main/java/com/stackroute/springboot/feedcomponent/SeedDataApplicationListener.java
+++ MuzixApplicationTrack/src/main/java/com/stackroute/springboot/feedcomponent/SeedDataApplicationListener.java
@@ -3,6 +3,7 @@ package com.stackroute.springboot.feedcomponent;
 
 import com.stackroute.springboot.domain.Track;
 import com.stackroute.springboot.exception.TrackAlreadyExistsException;
+import com.stackroute.springboot.repository.TrackRepo;
 import com.stackroute.springboot.service.TrackService;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,79 +22,14 @@ import org.springframework.stereotype.Component;
     public class SeedDataApplicationListener implements ApplicationListener<ContextRefreshedEvent>
 
     {
-
-        @Autowired
-
-        Environment environment;
-
-        @Autowired
-
-        TrackService trackService;
-
-        Track feedTrackData=new Track();
-
-
-
-        @Override
-
-        public void onApplicationEvent(ContextRefreshedEvent event)
-
-        {
-
-            feedTrackData.setTId(Integer.parseInt(environment.getProperty("song.1.tId")));
-
-            feedTrackData.setTName(environment.getProperty("song.1.tName"));
-
-            feedTrackData.setTComment(environment.getProperty("song.1.tComment"));
-
-            try {
-
-                trackService.saveTrack(feedTrackData);
-
-            } catch (TrackAlreadyExistsException e) {
-
-                e.printStackTrace();
-
-            }
-
-
-
-            feedTrackData.setTId(Integer.parseInt(environment.getProperty("song.2.tId")));
-
-            feedTrackData.setTName(environment.getProperty("song.2.tName"));
-
-            feedTrackData.setTComment(environment.getProperty("song.2.tComment"));
-
-            try {
-
-                trackService.saveTrack(feedTrackData);
-
-            } catch (TrackAlreadyExistsException e) {
-
-                e.printStackTrace();
-
-            }
-
-
-
-            feedTrackData.setTId(Integer.parseInt(environment.getProperty("song.3.tId")));
-
-            feedTrackData.setTName(environment.getProperty("song.3.tName"));
-
-            feedTrackData.setTComment(environment.getProperty("song.3.tComment"));
-
-            try {
-
-                trackService.saveTrack(feedTrackData);
-
-            } catch (TrackAlreadyExistsException e) {
-
-                e.printStackTrace();
-
-            }
-
-        }
-
+        private TrackRepo trackRepository;    @Autowired
+    public SeedDataApplicationListener(TrackRepo trackRepository) {
+        this.trackRepository = trackRepository;
+    }    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        trackRepository.save(new Track(11, "Alan Walker", "Good"));
+        trackRepository.save(new Track(44, "Sia", "Good"));
+    }
     }
 
 
